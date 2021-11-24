6632d40720351a14100a459e57850f583b90f632
sina <sinamohsenifar@gmail.com>
2021-11-24 18:48:55 +0330

    first serializer class

--- Serializers/Serializers/settings.py
+++ Serializers/Serializers/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'snippets.app.SnippetsConfig',
-    
+    'snippets.apps.SnippetsConfig',
+    'pygments',
+
 ]
 
 MIDDLEWARE = [
