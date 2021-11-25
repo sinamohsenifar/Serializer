3b3ba195ed88b8f1a8f4d76bf75dbb6b62771769
sina <sinamohsenifar@gmail.com>
2021-11-25 15:04:05 +0330

    class base views are done

--- Serializers/snippets/urls.py
+++ Serializers/snippets/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
-from snippets import views
+from .views import (snippet_detail, snippet_list, 
+                    SnippetListView, SnippetDetailView, 
+                    SnippetListGenericView, SnippetDetailGenericView, 
+                    SnippetGenericListCreateView, SnippetGenericRetrieveUpdateDestroyView)
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    #FUNCTION BASE VIEWS
+    path('snippets_func/', snippet_list),
+    path('snippets_func/<int:pk>/', snippet_detail),
+    #CLASS BASE VIEWS
+    path('snippets_class/', SnippetListView.as_view()),
+    path('snippets_class/<int:pk>', SnippetDetailView.as_view()),
+    #BASE GENERIC CLASS VIEWS
+    path('snippets_genericclass/', SnippetListGenericView.as_view()),
+    path('snippets_genericclass/<int:pk>', SnippetDetailGenericView.as_view()),
+    #GENERIC CLASS VIEWS
+    path('snippets_fullgenericclass/', SnippetGenericListCreateView.as_view()),
+    path('snippets_fullgenericclass/<int:pk>', SnippetGenericRetrieveUpdateDestroyView.as_view()),
+
 ]
--- Serializers/snippets/views.py
+++ Serializers/snippets/views.py
@@ -1,11 +1,22 @@
 from django.http import HttpResponse, JsonResponse
+from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from rest_framework.utils import serializer_helpers
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, SnippetModelSerializer
+from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from django.http import Http404
+from rest_framework import mixins
+from rest_framework import generics
 
 
+"""
+    FUNCTION BASE VIEWS TO WORK WITH REST API   this views are very old methods
+"""
+@api_view(['GET', 'POST'])
 @csrf_exempt
 def snippet_list(request):
     """
@@ -15,7 +26,7 @@ def snippet_list(request):
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return JsonResponse(serializer.data, safe=False, status=status.HTTP_201_CREATED)
     
     # for saving an object
     elif request.method == 'POST':
@@ -23,10 +34,12 @@ def snippet_list(request):
         serializer = SnippetSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse({'Job Done': True, 'status': 201})
-        return JsonResponse({'Job Done': False, 'status': 400})
+            return JsonResponse({'Job Done': True}, status=status.HTTP_201_CREATED)
+        return JsonResponse({'Job Done': False}, status=status.HTTP_400_BAD_REQUEST)
 
 
+
+@api_view(['GET', 'PUT', 'DELETE', 'PATCH'])
 @csrf_exempt
 def snippet_detail(request, pk):
     """
@@ -35,18 +48,14 @@ def snippet_detail(request, pk):
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
     print(snippet)
     
-    if request.method == 'DELETE':
-        print(request.method)
-        snippet.delete()
-        return HttpResponse(status=204)
-
-    elif request.method == 'GET':
+    if request.method == 'GET':
         print(request.method)
         serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data)
+        print(serializer.data)
+        return Response({'item': serializer.data}, status=status.HTTP_201_CREATED)
 
     elif request.method == 'PUT':
         print(request.method)
@@ -54,11 +63,124 @@ def snippet_detail(request, pk):
         serializer = SnippetSerializer(snippet, data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse({'Job Done': True, 'status': 201})
-        return JsonResponse({'Job Done': False, 'status': 400})
+            return Response({'Job Done': True, 'item': serializer.data}, status=status.HTTP_201_CREATED)
+        return Response({'Job Done': False}, status=status.HTTP_400_BAD_REQUEST)
 
-    
+    # this patch not working
+    elif request.method== 'PATCH':
+        data = JSONParser().parse(request)
+        serializer = SnippetModelSerializer(instance=snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'Job Done': True, 'item': serializer.data}, status=status.HTTP_201_CREATED)
+        return Response({'Job Done': False}, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+    elif request.method == 'DELETE':
+        print(request.method)
+        title = snippet.title
+        snippet.delete()
+        return Response({'Item-Delete-Done': True, 'item title': title}, status=status.HTTP_201_CREATED)
+
+
+
+"""
+    THE BASIC CLASS BASE VIEWS TO WORK WITH REST API   we are using class base views but with basic level the classes are better than this 
+"""
+
 class SnippetListView(APIView):
-    pass
+    """
+    List all snippets, or create a new snippet.
+    """
+    def get(self, request, format=None):
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'Job Done Response': True,'data_id': serializer.id}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class SnippetDetailView(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'Job Done Response': True,'data_id': serializer.id}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+"""
+    WE ARE USING CLASS BASE VIEWS TO WORK WITH REST API   but with lesser code , we are using mixins and generic api view , we can do lesser code
+"""
+
+
+class SnippetListGenericView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+
+class SnippetDetailGenericView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+"""
+    HERE WE ARE USING CLASS BASE VIEW WITH GENERIC LIST , CREATE , RETRIEVE , UPDATE AND DESTROY VIEWS WITH COMPRESSED CODING
+"""
+
+class SnippetGenericListCreateView(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetGenericRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
